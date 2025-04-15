2ce9260e01bcab090001d5cbf0180a3ec5ac8b10
barez <gbarez44@gmail.com>
2025-04-15 19:45:05 -0300

    feat: validate if user insert info

---
+++ main.js
@@ -0,0 +1,14 @@
+// Configura o acesso para a tela de login
+
+function acessar() {
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+    // Implementa a estrutura que testa se todos os campos foram preenchidos
+    
+    if(!loginEmail || !loginSenha) {
+        alert('Favor preencher os campos');
+    } else {
+        alert('Acesso liberado');
+    }
+}
