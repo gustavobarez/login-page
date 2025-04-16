b4914aa19a4f06c73f892c87bbe21a1b901635c2
barez <gbarez44@gmail.com>
2025-04-15 21:28:15 -0300

    feat: add updt table when new user

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function acessar() {
     let loginSenha = document.getElementById('loginSenha').value;
 
     // Implementa a estrutura que testa se todos os campos foram preenchidos
-    
     if(!loginEmail || !loginSenha) {
         alert('Favor preencher os campos');
     } else {
@@ -21,6 +20,21 @@ function salvarUser() {
 
     if(nomeUser) {
         dadosLista.push(nomeUser);
+        criaLista();
+        console.log('salvarUser if')
         document.getElementById('nomeUser').value = '';
+    } else {
+        alert('Favor informar um nome para cadastro');
+    }
+}   
+
+// funcao para preencher a lista de cadastro
+function criaLista() {
+    let tabela = "<tr><td>Nome</td><th>Actions</th></tr>";
+    for (let i = 0;i <= (dadosLista.length -1);i++) {
+        tabela += "<tr><td>" + dadosLista[i] + "</td><button class='btn btn-warning'>Editar</button><button class='btn btn-danger'>Excluir</button><td>Actions</td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+        console.log('chegou');
     }
 }
+
