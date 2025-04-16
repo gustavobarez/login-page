30887c10c2656458a3646f9a63d741fa07b5e88d
barez <gbarez44@gmail.com>
2025-04-15 21:47:41 -0300

    feat: add edit to change user name

--- main.js
+++ main.js
@@ -31,14 +31,19 @@ function salvarUser() {
 function criaLista() {
     let tabela = "<tr><th>Nome</th><th>Actions</th></tr>";
     for (let i = 0;i <= (dadosLista.length -1);i++) {
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning'>Editar</button></td><td><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button></td><td><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
 }
 
 // funcao para excluir nome da lista
-
 function excluir(i) {
     dadosLista.splice((i-1), 1);
     document.getElementById('tabela').deleteRow(i);   
+}
+
+// funcao para editar o nome da lista
+function editar(i) {
+    document.getElementById('nomeUser').value = dadosLista[(i-1)];
+    dadosLista.splice(dadosLista[(i-1)], 1);
 }
